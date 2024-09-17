3b09fe011fd88f82a0c1c57ea92eea12ba51ecf6
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-09-17 23:40:23 +0700

    fix: remove some file

--- src/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-import User from '../models/userModel.js';
-import asyncHandler from '../middlewares/asyncHandler.js';
-
-const createUser = asyncHandler(async(req, res)=>{
-    res.send('test');
-});
-
-export {createUser};
