0dedc0c89c2d73db1510e2d90ef2feb26cb625d0
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-10-18 00:50:21 +0700

    refactor: clean up user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,31 @@
-import express from 'express';
-import { createUsers} from '../controllers/Users/createUsers.js';
-import { loginUsers } from '../controllers/Users/loginUsers.js';
-import { logoutUsers } from '../controllers/Users/logoutUsers.js';
+import express from "express";
+import { authenticate, authorizeAdmin } from "../middlewares/auth.js";
+import { createUsers } from "../controllers/Users/createUsers.js";
+import { loginUsers } from "../controllers/Users/loginUsers.js";
+import { logoutUsers } from "../controllers/Users/logoutUsers.js";
+import { getUsers } from "../controllers/Users/getUsers.js";
+import { getCurrentUser } from "../controllers/Users/getCurrentUser.js";
+import { updateUsers } from "../controllers/Users/updateUsers.js";
+import { deleteUsers } from "../controllers/Users/deleteUsers.js";
+import { getUserById } from "../controllers/Users/getUserById.js";
+import { updateUserById } from "../controllers/Users/updateUserById.js";
 const router = express.Router();
 
-router.route('/').post(createUsers);
-router.post('/auth', loginUsers);
-router.post('/logout', logoutUsers);
+router.route("/").post(createUsers).get(authenticate, authorizeAdmin, getUsers);
 
+router.post("/auth", loginUsers);
+router.post("/logout", logoutUsers);
+
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUser)
+  .put(authenticate, updateUsers);
+
+//admin
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUsers)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
